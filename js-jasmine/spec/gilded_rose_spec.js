ca594c4e27d640e49c7d59993f28c8e013909a4f
Daniel Ericsson <dgesciguy@gmail.com>
2019-05-23 12:12:23 -0700

    Refactor gilded rose to use special 'item updaters' for special item types.

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,124 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+var {
+  Shop,
+  Item 
+} = require("../src/gilded_rose.js");
+// Remove `Shop` from the above import and uncomment the below to test the original.
+// Note that the original Shop does not support Conjured-type items, so those tests will fail.
+// var { Shop } = require("../src/original_gilded_rose.js");
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+describe("Gilded Rose Refactored Version", function() {
+  it("should update a generic item: sellIn decrements by 1, quality decrements by 1", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 3)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].sellIn).toEqual(0);
+    expect(items[0].quality).toEqual(2);
   });
 
+  it("should update a generic item: when sellIn is 0 or less, quality decrements by 2", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 3)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(-1);
+    expect(items[0].quality).toEqual(1);
+  });
+
+  it("should update a generic item: when sellIn is 0 or less, quality decrements by 2 and not to below 0", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(-1);
+    expect(items[0].quality).toEqual(0);
+  });
+
+  describe("aged brie", () => {
+    it("quality should always increase", () => {
+      const gildedRose = new Shop([new Item("Aged Brie", 1, 2)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(0);
+      expect(items[0].quality).toEqual(3);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(-1);
+      expect(items[0].quality).toEqual(5);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(-2);
+      expect(items[0].quality).toEqual(7);
+    });
+
+    it("quality should always increase, but never to above 50", () => {
+      const gildedRose = new Shop([new Item("Aged Brie", 1, 49)]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(0);
+      expect(items[0].quality).toEqual(50);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(-1);
+      expect(items[0].quality).toEqual(50);
+    });
+  });
+
+  it("should update Sulfuras: as a legendary item, it never changes", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80)
+    ]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(0);
+    expect(items[0].quality).toEqual(80);
+    items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(0);
+    expect(items[0].quality).toEqual(80);
+  });
+
+  describe("backstage passes", () => {
+    it("should increase in quality as sellIn decreases", () => {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 12, 12)
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(11);
+      expect(items[0].quality).toEqual(13);
+    });
+
+    it("should increase in quality by 2 when 10 >= sellIn > 5", () => {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 12)
+      ]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(9);
+      expect(items[0].quality).toEqual(14);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(8);
+      expect(items[0].quality).toEqual(16);
+    });
+
+    it("should increase in quality by 3 when 5 >= sellIn > 0", () => {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 6, 20)
+      ]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(5);
+      expect(items[0].quality).toEqual(22);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(4);
+      expect(items[0].quality).toEqual(25);
+    });
+
+    it("should have quality 0 when sellIn has passed", () => {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20)
+      ]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(0);
+      expect(items[0].quality).toEqual(23);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(-1);
+      expect(items[0].quality).toEqual(0);
+    });
+  });
+
+  it("should degrade Conjured items at double the standard quality per day", () => {
+    const gildedRose = new Shop([new Item("Conjured treasure chest", 1, 20)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(0);
+    expect(items[0].quality).toEqual(18);
+    items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(-1);
+    expect(items[0].quality).toEqual(14);
+  });
 });
--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -1,3 +1,4 @@
+// DO NOT MODIFY UNDER PAIN OF GOBLIN INSTA-RAGE ONE-SHOT (yikes)
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
@@ -5,58 +6,101 @@ class Item {
     this.quality = quality;
   }
 }
+// END DO NOT MODIFY
+
+/**
+ * Most generic items follow these standard rules for item updates:
+ *  1. `quality` decreases by 1 for every day while `sellIn` is greater than today (0);
+ *  2. `quality` decreases by 2 for every day thereafter.
+ *  3. `sellIn` decrements by 1 every day.
+ */
+const standardItemUpdater = (item) => {
+  if (item.sellIn > 0) {
+    item.quality--;
+  } else {
+    item.quality -= 2;
+  }
+  if (item.quality < 0) item.quality = 0;
+  item.sellIn--;
+  return item;
+}
+
+/**
+ * Map of item types that require specific handling rules to an updater that
+ * implements those rules.
+ * Each key should be an all-lower-case string that is expected to be found
+ * in the `name` of each item, and each value should be a function that takes a
+ * non-null Item object, updates it according to the rules for that item type,
+ * and returns it.
+ */
+const customItemUpdaters = {
+  // Aged brie does not decrease in quality daily, instead it increases the same amount.
+  // Max quality for brie is 50.
+  "aged brie": (item) => {
+    if (item.quality < 50) {
+      if (item.sellIn > 0) {
+        item.quality++;
+      } else {
+        item.quality += 2;
+      }
+    }
+    item.sellIn--;
+    return item;
+  },
+  // Sulfuras is a legendary item and is not updated.
+  "sulfuras": (item) => item,
+  // Backstage passes increase in quality daily until expiring, following these rules:
+  //   sellIn > 10: quality increases by 1
+  //   10 >= sellIn >= 5: increases by 2
+  //   5 > sellIn => 0: increases by 3
+  //   0 > sellIn: equal to 0
+  "backstage passes": (item) => {
+    item.sellIn--;
+    if (item.sellIn > 10) {
+      item.quality++;
+    } else if (item.sellIn >= 5) {
+      item.quality += 2;
+    } else if (item.sellIn >= 0) {
+      item.quality += 3;
+    } else {
+      item.quality = 0;
+    }
+    if (item.quality > 50) item.quality = 50;
+    return item;
+  },
+  // Conjured items have their quality degrade twice as quickly as normal.
+  "conjured": (item) => {
+    if (item.sellIn > 0) {
+      item.quality -= 2;
+    } else {
+      item.quality -= 4;
+    }
+    if (item.quality < 0) item.quality = 0;
+    item.sellIn--;
+    return item;
+  }
+}
+
+const customItemTypes = Object.keys(customItemUpdaters);
 
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
+  
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
+      const nextItem = this.items[i];
+      let updater = standardItemUpdater;
+      // Check for a special updater matching this item's name.
+      for (var i in customItemTypes) {
+        if (nextItem.name.toLowerCase().includes(customItemTypes[i])) {
+          updater = customItemUpdaters[customItemTypes[i]];
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+      // Apply the updater (standard or whatever we found in the map) to the item.
+      this.items[i] = updater(nextItem);
     }
-
     return this.items;
   }
 }
